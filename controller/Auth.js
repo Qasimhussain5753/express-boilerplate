ac13ebd7e3b6bc532898b81574c1fbac7a837f3b
Qasimhussain5753 <58785338+Qasimhussain5753@users.noreply.github.com>
2020-11-21 11:55:05 +0500

    Client and serviceProvider added

--- controller/Auth.js
+++ controller/Auth.js
@@ -6,80 +6,183 @@ admin.initializeApp({
 const db = admin.firestore();
 
 exports.signup = async (req, res, next) => {
-  console.log(req.body);
-  const user = await db.collection("Users").doc(req.body.uid).get();
-  console.log(user.exists);
-  if (user.exists) {
-    console.log("already");
-    res.status(500).send({
-      success: false,
-      message: "User already exists.",
-    });
-  } else {
-    console.log("Signing up");
-    await db
-      .collection("Users")
-      .doc(req.body.uid)
-      .set({ ...req.body })
-      .then((r) => {})
-      .catch((err) => {
-        console.log("Error setting", err);
+  const { user, uid } = req.body;
+  if (user === "client") {
+    const client = await db.collection("Clients").doc(req.body.uid).get();
+    console.log(client.exists);
+    if (client.exists) {
+      console.log("already");
+      res.status(500).send({
+        success: false,
+        message: "client already exists.",
       });
-    res.status(200).send({
-      success: true,
-      message: "Successfully signed up.",
-      data: req.body,
-    });
+    } else {
+      console.log("Signing up");
+      await db
+        .collection("Clients")
+        .doc(req.body.uid)
+        .set({ ...req.body })
+        .catch((err) => {
+          console.log("Error setting", err);
+        });
+      res.status(200).send({
+        success: true,
+        message: "Successfully signed up.",
+        data: req.body,
+      });
+    }
+  }
+  if (user === "serviceProvider") {
+    console.log("serviceProvider");
+    const serviceprovider = await db
+      .collection("ServiceProvider")
+      .doc(req.body.uid)
+      .get();
+    if (serviceprovider.exists) {
+      res.status(500).send({
+        success: false,
+        message: "Service Provider already exists.",
+      });
+    } else {
+      console.log("Signing up");
+      await db
+        .collection("ServiceProvider")
+        .doc(req.body.uid)
+        .set({ ...req.body })
+        .catch((err) => {
+          console.log("Error setting", err);
+        });
+      res.status(200).send({
+        success: true,
+        message: "Successfully signed up.",
+        data: req.body,
+      });
+    }
   }
 };
 
 exports.signin = async (req, res, next) => {
-  const { uid, fcmToken } = req.body;
-  const user = await db.collection("Users").doc(uid).get();
-
-  if (user.exists) {
-    try {
-      await db.collection("Users").doc(uid).set({ fcmToken }, { merge: true });
-      return res.status(200).send({
-        success: true,
-        message: "Successfully Login.",
-        data: user.data(),
-      });
-    } catch (error) {
-      return res.status(500).send({
-        error: "error",
+  const { uid, fcmToken, user } = req.body;
+  if (user === "client") {
+    const client = await db.collection("Clients").doc(uid).get();
+    if (client.exists) {
+      try {
+        await db
+          .collection("Clients")
+          .doc(uid)
+          .set({ fcmToken }, { merge: true });
+        return res.status(200).send({
+          success: true,
+          message: "Successfully Login.",
+          data: client.data(),
+        });
+      } catch (error) {
+        return res.status(500).send({
+          error: "error",
+        });
+      }
+    } else {
+      res.status(500).send({
+        success: false,
+        message: "client not Found",
       });
     }
-  } else {
-    res.status(500).send({
-      success: false,
-      message: "User not Found",
-    });
+  }
+  if (user === "serviceProvider") {
+    const serviceprovider = await db
+      .collection("ServiceProvider")
+      .doc(uid)
+      .get();
+    if (serviceprovider.exists) {
+      try {
+        await db
+          .collection("ServiceProvider")
+          .doc(uid)
+          .set({ fcmToken }, { merge: true });
+        return res.status(200).send({
+          success: true,
+          message: "Successfully Login.",
+          data: serviceprovider.data(),
+        });
+      } catch (error) {
+        return res.status(500).send({
+          error: "error",
+        });
+      }
+    } else {
+      res.status(500).send({
+        success: false,
+        message: "Service Provider not Found",
+      });
+    }
   }
 };
 
-exports.logout = async (req,res, next) =>{
-  const { uid } = req.body;
- const user = await db.collection("Users").doc(uid).get();
- 
-   if(user.data().fcmToken === req.body.fcmToken){
-       try {
-           await db.collection("Users").doc(uid).set({fcmToken: ""}, {merge: true});
-           return res.status(200).send({
-               success: true,
-               message: "Logout successfully.",
-            })
+exports.logout = async (req, res, next) => {
+  const { uid, user } = req.body;
+  if (user === "client") {
+    const client = await db.collection("Clients").doc(uid).get();
+    if (client.exists) {
+      if (client.data().fcmToken === req.body.fcmToken) {
+        console.log("fcmToken");
+        try {
+          await db
+            .collection("Clients")
+            .doc(uid)
+            .set({ fcmToken: "" }, { merge: true });
+          return res.status(200).send({
+            success: true,
+            message: "Logout successfully.",
+          });
+        } catch (error) {
+          return res.status(500).send({
+            error: "error",
+          });
         }
-        catch(error){
-            return res.status(500).send({
-                error: "error"
-            })
+      } else {
+        res.status(200).send({
+          success: true,
+          message: "Successfully Logout",
+        });
+      }
+    } else {
+      res.status(200).send({
+        success: true,
+        message: "Successfully logout.",
+      });
+    }
+  }
+  if (user === "serviceProvider") {
+    console.log("fcmToken");
+    const serviceprovider = await db.collection("ServiceProvider").doc(uid).get();
+    if (serviceprovider.exists) {
+      if (serviceprovider.data().fcmToken === req.body.fcmToken) {
+       
+        try {
+          await db
+            .collection("ServiceProvider")
+            .doc(uid)
+            .set({ fcmToken: "" }, { merge: true });
+          return res.status(200).send({
+            success: true,
+            message: "Logout successfully.",
+          });
+        } catch (error) {
+          return res.status(500).send({
+            error: "error",
+          });
         }
-   }
-   else {
-       res.status(200).send({
-           success: true,
-           message: "Successfully Logout"
-       })
-   }
-}
+      } else {
+        res.status(200).send({
+          success: true,
+          message: "Successfully Logout",
+        });
+      }
+    } else {
+      res.status(200).send({
+        success: true,
+        message: "Successfully logout.",
+      });
+    }
+  }
+};
